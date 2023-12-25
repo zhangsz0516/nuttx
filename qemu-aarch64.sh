6ee9c4f0b57b54a7d165d5cfb14f7a134e696297
张世争 <zhangshizheng@rt-thread.com>
2023-12-25 18:28:20 +0800

    qemu-armv8a gdb 调试

---
+++ qemu-aarch64.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+qemu-system-aarch64 -cpu cortex-a53 -nographic \
+    -machine virt,virtualization=on,gic-version=3 \
+    -net none -chardev stdio,id=con,mux=on -serial chardev:con \
+    -mon chardev=con,mode=readline -kernel ./nuttx
